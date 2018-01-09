82f37658cfa1f8bd7fa52f592ef3dc5c601a4451
zyl-GitHub <zyldomain@163.com>
2018-01-09 23:42:29 +0800

    前台环境的搭建完成

---
+++ CM-manager/CM-manager-Service/src/main/java/com/CM/component/JedisClient.java
@@ -0,0 +1,16 @@
+package com.CM.component;
+
+public interface JedisClient
+{
+	//增
+	public String set(String key,String value);
+	//查
+	public String get(String key);
+	public Long hset(String key,String item,String value);
+	public String hget(String key,String item);
+	public Long incr(String key);
+	public Long decr(String key);
+	public Long expire(String key,int second);
+	public Long ttl(String key);
+	public Long hdel(String key,String item);
+}
---
+++ CM-sso/src/main/java/com/CM/sso/component/Impl/JedisClientSingle.java
@@ -0,0 +1,86 @@
+package com.CM.sso.component.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.CM.sso.component.JedisClient;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class JedisClientSingle implements JedisClient
+{
+	@Autowired
+	private JedisPool jedisPool;
+	@Override
+	public String set(String key, String value)
+	{
+		Jedis jedis = jedisPool.getResource();
+		value = jedis.set(key, value);
+		return value;
+	}
+
+	@Override
+	public String get(String key)
+	{
+		Jedis jedis = jedisPool.getResource();
+		String value = jedis.get(key);
+		return value;
+	}
+
+	@Override
+	public Long hset(String key, String item, String value)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long result = jedis.hset(key, item, value);
+		return result;
+	}
+
+	@Override
+	public String hget(String key, String item)
+	{
+		Jedis jedis = jedisPool.getResource();
+		String value = jedis.hget(key, item);
+		return value;
+	}
+
+	@Override
+	public Long incr(String key)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long value = jedis.incr(key);
+		return value;
+	}
+
+	@Override
+	public Long decr(String key)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long value = jedis.decr(key);
+		return value;
+	}
+
+	@Override
+	public Long expire(String key, int second)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long value = jedis.expire(key, second);
+		return value;
+	}
+
+	@Override
+	public Long ttl(String key)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long value = jedis.ttl(key);
+		return value;
+	}
+
+	@Override
+	public Long hdel(String key, String item)
+	{
+		Jedis jedis = jedisPool.getResource();
+		Long value = jedis.hdel(key, item);
+		return value;
+	}
+
+}
